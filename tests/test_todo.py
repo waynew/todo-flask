3cb3c7b50e4c1b6370d63e21b23dca0ec56e9313
Wayne Werner <wayne@waynewerner.com>
2015-03-01 13:04:53 -0600

    Can save a todo item & get it from db

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock as mock
 import todo.todo as todo
 
 
@@ -18,3 +19,45 @@ class TestTodo(unittest.TestCase):
         item = todo.Todo(description='fnord')
 
         self.assertFalse(item.completed, "Completed should be False")
+
+    @mock.patch('todo.data_access.add_item')
+    def test_when_todo_save_is_called_id_should_be_set(self, mock_add_item):
+        item = todo.Todo(description='whatever')
+
+        item.save()
+
+        self.assertIsNotNone(item.id, "Item id should be set")
+
+    @mock.patch('todo.data_access.add_item')
+    def test_when_todo_save_is_called_id_should_be_set_to_result_of_data_access_call(self, mock_add_item):
+        expected_id = 42
+
+        mock_add_item.return_value = expected_id
+        item = todo.Todo(description='your father smelled of elderberry')
+
+        item.save()
+
+        self.assertEqual(expected_id, item.id,
+                         'Item id should have been set to result of call')
+
+    @mock.patch('todo.data_access.get_item')
+    def test_when_todo_get_item_is_called_and_data_access_returns_None_it_should_return_None_too(self, mock_get_item):
+        mock_get_item.return_value = None
+
+        self.assertIsNone(todo.get_item(42),
+                          'get_item should have returned None')
+
+    @mock.patch('todo.data_access.get_item')
+    def test_when_todo_get_item_is_called_and_data_access_returns_data_Todo_should_have_same_values(self, mock_get_item):
+        expected_id = 1337
+        expected_desc = "h4><0r"
+        expected_completed = False
+        mock_get_item.return_value = (expected_id,
+                                      expected_desc,
+                                      expected_completed)
+
+        item = todo.get_item(expected_id)
+
+        self.assertEqual(expected_id, item.id)
+        self.assertEqual(expected_desc, item.description)
+        self.assertEqual(expected_completed, item.completed)
--- todo/todo.py
+++ todo/todo.py
@@ -1,8 +1,29 @@
+from . import data_access
 
 class Todo:
     '''Todo is just this thing, you know?'''
 
-    def __init__(self, description):
-        self.id = None
+    def __init__(self, description, id=None, completed=False):
+        self.id = id
         self.description = description
-        self.completed = False
+        self.completed = completed
+
+    def save(self):
+        '''Save this item and set the .id property.'''
+        self.id = data_access.add_item(self.description)
+
+
+def get_item(id):
+    '''Get the Todo item that has the provided id. If no item
+    exists with that id, return None.
+    '''
+
+    data = data_access.get_item(id)
+    if data is not None:
+        item = Todo(id=data[0],
+                    description=data[1],
+                    completed=data[2],
+                    )
+        return item
+    else:
+        return None
