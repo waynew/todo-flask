6574a0fcf7424383cbaa5867a5ed02089525d904
Wayne Werner <wayne@waynewerner.com>
2015-03-01 11:55:35 -0600

    Todo class is created nicely

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -2,6 +2,19 @@ import unittest
 import todo.todo as todo
 
 
-class TestServer(unittest.TestCase):
-    def test_a_thing(self):
-        self.assertTrue(True, "Yeah, this was wrong anyway")
+class TestTodo(unittest.TestCase):
+    def test_when_todo_is_created_the_id_should_be_None(self):
+        item = todo.Todo('ignore this description')
+
+        self.assertIsNone(item.id, "New item should have id of None")
+
+    def test_when_todo_is_created_description_should_be_set(self):
+        expected_desc = 'This is the most todo-y todo ever'
+        item = todo.Todo(description=expected_desc)
+
+        self.assertEqual(expected_desc, item.description)
+
+    def test_when_todo_is_created_completed_should_be_False(self):
+        item = todo.Todo(description='fnord')
+
+        self.assertFalse(item.completed, "Completed should be False")
---
+++ todo/todo.py
@@ -0,0 +1,8 @@
+
+class Todo:
+    '''Todo is just this thing, you know?'''
+
+    def __init__(self, description):
+        self.id = None
+        self.description = description
+        self.completed = False
